16c8347b7e7db350c46e4f8ae43967fbe7641cf4
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2024-10-25 01:39:37 +0600

    Created Header component and added to page file

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,9 @@
-import Image from "next/image";
+import Header from "@/components/Header/Header";
 
 export default function Home() {
   return (
-    <div className="p-10 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col items-center justify-center">
-        <h1>NEXT.JS</h1>
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-      </main>
-    </div>
+    <main className="">
+      <Header />
+    </main>
   );
 }
