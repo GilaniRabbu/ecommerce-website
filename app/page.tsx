d344d75211f3e8a807426e7897e6260fc465f7eb
devabdulkader <devabdulemail@gmail.com>
2024-10-26 16:33:10 +0600

    added test swiper

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/Header/Header";
+import TestSwiper from "@/components/Header/TestSwiper";
 
 export default function Home() {
   return (
     <main className="">
       <Header />
+      <TestSwiper />
     </main>
   );
 }
