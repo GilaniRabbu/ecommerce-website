cc0b18eed3c684bf1910a9af95c4299a5bcd125c
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2024-12-10 16:48:39 +0600

    updated code

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import DailyDeals from "@/components/DailyDeals/DailyDeals";
 import Header from "@/components/Header/Header";
 import HeroSection from "@/components/Hero/Hero";
 import ProductSection from "@/components/Product/Product";
-import Favorites from "@/pages/favorites";
+// import Favorites from "@/pages/favorites";
 
 export default function Home() {
   return (
@@ -13,7 +13,7 @@ export default function Home() {
         <HeroSection />
         <ProductSection />
         <DailyDeals />
-        <Favorites />
+        {/* <Favorites /> */}
       </Suspense>
     </main>
   );
